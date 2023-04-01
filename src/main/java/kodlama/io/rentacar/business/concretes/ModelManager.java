dacaf42e37afa43ac6572e83cf066324000529d7
Hayrullah Cagil <cagilhayrullah@gmail.com>
2023-04-01 12:20:45 +0300

    add model update

--- src/main/java/kodlama/io/rentacar/business/concretes/ModelManager.java
+++ src/main/java/kodlama/io/rentacar/business/concretes/ModelManager.java
@@ -2,9 +2,11 @@ package kodlama.io.rentacar.business.concretes;
 
 import kodlama.io.rentacar.business.abstracts.ModelService;
 import kodlama.io.rentacar.business.dto.requests.create.CreateModelRequest;
+import kodlama.io.rentacar.business.dto.requests.update.UpdateModelRequest;
 import kodlama.io.rentacar.business.dto.responses.create.CreateModelResponse;
 import kodlama.io.rentacar.business.dto.responses.get.GetAllModelsResponse;
 import kodlama.io.rentacar.business.dto.responses.get.GetModelResponse;
+import kodlama.io.rentacar.business.dto.responses.update.UpdateModelResponse;
 import kodlama.io.rentacar.enttities.Brand;
 import kodlama.io.rentacar.enttities.Model;
 import kodlama.io.rentacar.repository.BrandRepository;
@@ -48,6 +50,18 @@ public class ModelManager implements ModelService {
         model.setBrand(brand);
         repository.save(model);
         CreateModelResponse response = mapper.map(model, CreateModelResponse.class);
+        return response;
+    }
+
+    @Override
+    public UpdateModelResponse update(int id, UpdateModelRequest request) {
+        checkIfBrandExist(id);
+        Model model = mapper.map(request,Model.class);
+        model.setId(id);
+        Brand brand = brandRepository.findById(request.getBrandId()).orElseThrow();
+        model.setBrand(brand);
+        repository.save(model);
+        UpdateModelResponse response = mapper.map(model, UpdateModelResponse.class);
         return response;
     }
 
